351aca59e6bcceed537687723b851f42ad72c153
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-07-04 20:23:12 +0200

    update 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,39 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+import { Notify } from 'notiflix';
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  let step = Number(e.currentTarget.step.value);
+  let amount = Number(e.currentTarget.amount.value);
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
+  }
+}
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  let promiseValue = { position, delay };
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(promiseValue);
+    }
+    reject(promiseValue);
+  });
+}
